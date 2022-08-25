043b257d72d7dddbdce2250ba692ecb211303745
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-25 14:42:55 +0300

    Add task 044 solution

---
+++ Task044/Program.cs
@@ -0,0 +1,19 @@
+﻿// Не используя рекурсию, вывведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+void Fibonacci(int num)
+{
+    int fnum1 = 0;
+    int fnum2 = 1;
+    int temp;
+
+    Console.Write($"{fnum1} ");
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"{fnum2} ");
+        temp = fnum2;
+        fnum2 = fnum2 + fnum1;
+        fnum1 = temp;
+    }
+}
+
+Fibonacci(25);
